846321964cd622d0805c5c3842fff0c455c7b070
Antonio Kim <antoniok@sfu.ca>
2024-08-07 21:31:39 -0700

    adding pie chart and line chart

--- CabinLogInternal/src/features/dashboard/DashboardLayout.tsx
+++ CabinLogInternal/src/features/dashboard/DashboardLayout.tsx
@@ -4,6 +4,8 @@ import Spinner from '../../ui/Spinner';
 import { useRecentStays } from './useRecentStays';
 import Stats from './Stats';
 import { useCabins } from '../cabins/useCabins';
+import SalesChart from './SalesChart';
+import DurationChart from './DurationChart';
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -28,8 +30,8 @@ function DashboardLayout() {
         cabinCount={cabins}
       />
       <div>Today's activity</div>
-      <div>Chart stay durations</div>
-      <div>Chart sales</div>
+      <DurationChart confirmedStays={confirmedStays} />
+      <SalesChart bookings={bookings} numDays={numDays} />
     </StyledDashboardLayout>
   );
 }
